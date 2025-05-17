7a34f83b7d94507a796fbf96abf328069c739b1c
Arnaud <arnaudadjovi274@gmail.com>
2025-05-17 10:26:48 +0100

    Refactor GET function in route.ts to return setups with status code, improving API response structure and error handling.

--- src/app/api/setups/route.ts
+++ src/app/api/setups/route.ts
@@ -3,9 +3,8 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   try {
-    console.log('setups from api')
     const setups = await getSetups()
-    return NextResponse.json(setups)
+    return NextResponse.json({ status: 200, setups })
   } catch (error) {
     console.error('Erreur lors du chargement des setups:', error)
     return NextResponse.json({ error: 'Erreur lors du chargement des setups' }, { status: 500 })
